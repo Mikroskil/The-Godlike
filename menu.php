b4df1fdf8f6fc39a83a715a4b1edce0aa080dcd2
Stanley <stanz_coolz@yahoo.com>
2013-12-18 15:58:26 +0700

    menu.php

---
+++ menu.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen"> 
+    <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen">
+    <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/cufon-replace.js" type="text/javascript"></script> 
+    <script src="js/Dynalight_400.font.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script>  
+    <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script> 
+    <script src="js/hover-image.js" type="text/javascript"></script>
+    <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>  
+    <script src="js/jquery.bxSlider.js" type="text/javascript"></script> 
+    <script type="text/javascript">
+		$(document).ready(function() {
+			$('#slider-2').bxSlider({
+				pager: true,
+				controls: false,
+				moveSlideQty: 4,
+				displaySlideQty: 4
+			});
+			$("a[data-gal^='prettyPhoto']").prettyPhoto({theme:'facebook'});
+		}); 
+	</script>
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tr>
+    <td colspan="3">
+		<table width="100%" height="100%">
+  <tr>
+    <td width="122" rowspan="2"><img src="images/panda cafe 2.jpg" width="120" height="113" /></td>
+    <td width="857" height="48"> 
+      <table width="454" align="center">
+        <tr bgcolor="#FFCCFF">
+          <td width="444" height="47" align="center"><b><font size="+3"><marquee>WELCOME TO PANDA CAFE</marquee></font></b></td>
+        </tr>
+      </table>    
+	</td>
+  </tr>
+  <tr>
+    <td height="52"><table width="817" height="31" align="center" cellspacing="8" cellpadding="4">
+      <tr align="center">
+	  	<td width="20%"><a class="button-2" href="index.php"><font size="+1" color="#000000"><b> Home </b></font></a></td>
+        <td width="20%"><a class="button-3" href="menu.php"><font size="+1" color="#000000"><b> Menu </b></font></a></td>
+        <td width="20%"><a class="button-2" href="#"><font size="+1" color="#000000"><b> Promotion And Discount </b></font></a></td>
+        <td width="20%"><a class="button-2" href="billing.php"><font size="+1" color="#000000"><b> Billing </b></font></a></td>
+        <td width="20%"><a class="button-2" href="forum.php"><font size="+1" color="#000000"><b> Forum </b></font></a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+	</td>
+  </tr>
+  
+  <tr>
+    <td>
+		
+    <section id="content">
+        <div class="main">
+            <div class="container">
+                <div id="slider-2">
+                	<div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img1.jpg" alt=""></a></figure>
+                            <h5>bla bla bla </h5>
+                            <p class="p1">bla bla bla.</p>
+                            <p class="p2"><strong class="color-2">Price: Rp. </strong></p>
+                                                   </div>
+                        <figure><a class="lightbox-image" href="images/slider-img2.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img5.jpg" alt=""></a></figure>
+                        <h5>bla bla bla </h5>
+                        <p class="p1">bla bla bla</p>
+                        <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                            </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img3.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img2.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img6.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img3.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img2.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img7.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img3.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img4.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img8.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img1.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img2.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img5.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img3.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img2.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img6.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img3.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img2.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img7.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img3.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img4.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img8.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img1.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img2.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img5.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                    <div>
+                    	<div class="p4">
+                            <figure><a class="lightbox-image" href="images/slider-img3.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img2.jpg" alt=""></a></figure>
+                            <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                        <figure><a class="lightbox-image" href="images/slider-img1.jpg" data-gal="prettyPhoto[prettyPhoto]"><img src="images/page3-img6.jpg" alt=""></a></figure>
+                        <h5>bla bla bla</h5>
+                            <p class="p1">bla bla bla</p>
+                            <p class="p2"><strong class="color-2">Price: Rp.</strong></p>
+                                                    </div>
+                </div>
+				
+				Nomor Meja : <input type="text" /> <br /> <br />
+				Password : <input type="text" /> <br />
+				
+				<form method="post" action="menu2.php">
+				<input type="submit" value="Login"/>
+            	</form>
+			</div>
+        </div>
+    </section>
+	</td>
+  </tr>
+</table>
+</body>
+</html>
