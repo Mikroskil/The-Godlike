5cea268fb2a4fdb99fc310b77e3aac34577c4668
Stanley <stanz_coolz@yahoo.com>
2013-12-18 16:06:15 +0700

    forum.php

---
+++ forum.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tr>
+    <td>
+		<table width="100%" height="100%">
+  <tr>
+    <td width="122" rowspan="2"><img src="images/panda cafe 2.jpg" width="120" height="113" /></td>
+    <td width="857" height="48"> 
+      <table width="454" align="center">
+        <tr bgcolor="#FFCCFF">
+          <td width="444" height="47" align="center"><b><font size="+3"><marquee>WELCOME TO PANDA CAFE</marquee></font></b></td>
+        </tr>
+      </table>	</td>
+  </tr>
+  <tr>
+    <td height="52"><table width="817" height="31" align="center" cellspacing="8" cellpadding="4">
+      <tr align="center">
+	  	<td width="20%"><a class="button-2" href="index.php"><font size="+1" color="#000000"><b> Home </b></font></a></td>
+        <td width="20%"><a class="button-2" href="menu.php"><font size="+1" color="#000000"><b> Menu </b></font></a></td>
+        <td width="20%"><a class="button-2" href="#"><font size="+1" color="#000000"><b> Promotion And Discount </b></font></a></td>
+        <td width="20%"><a class="button-2" href="billing.php"><font size="+1" color="#000000"><b> Billing </b></font></a></td>
+        <td width="20%"><a class="button-3" href="forum.php"><font size="+1" color="#000000"><b> Forum </b></font></a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>	</td>
+  </tr>
+  
+  <tr>
+    <td>
+	  <form id="form1" name="form1" method="post" action="forum2.php">
+      <label>
+      <textarea id="txt1" name="textarea" cols="122" rows="20" readonly="readonly">
+	  </textarea>
+      </label>
+	    <label>
+	    <textarea id="txt2" name="textarea2" cols="70" rows="4"></textarea>
+	    </label>
+		<br /><br />
+		
+		ID : <input type="text" /> <br /> <br />
+		Password : <input type="text" /><br /> <br />
+		
+				<input type="submit" value="Login"/>
+		
+	  </form>    </td>
+  </tr>
+</table>
+</body>
+</html>
