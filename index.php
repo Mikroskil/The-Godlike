d043f3c889bdbe50aae8888447d40f5dc8c2d472
Stanley <stanz_coolz@yahoo.com>
2013-12-18 15:46:51 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/cufon-replace.js" type="text/javascript"></script> 
+    <script src="js/Dynalight_400.font.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script>
+    <script src="js/tms-0.3.js" type="text/javascript"></script>
+    <script src="js/tms_presets.js" type="text/javascript"></script>
+    <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+    <script src="js/jquery.equalheights.js" type="text/javascript"></script>
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tr>
+    <td colspan="3">
+		<table width="100%" height="100%">
+  <tr>
+    <td width="122" rowspan="2"><img src="images/panda cafe 2.jpg" width="120" height="113" /></td>
+    <td width="857" height="48"> 
+      <table width="454" align="center">
+        <tr bgcolor="#FFCCFF">
+          <td width="444" height="47" align="center"><b><font size="+3"><marquee>WELCOME TO PANDA CAFE</marquee></font></b></td>
+        </tr>
+      </table>    
+	</td>
+  </tr>
+  <tr>
+    <td height="52"><table width="817" height="31" align="center" cellspacing="8" cellpadding="4">
+      <tr align="center">
+	  	<td width="20%"><a class="button-3" href="index.php"><font size="+1" color="#000000"><b> Home </b></font></a></td>
+        <td width="20%"><a class="button-2" href="menu.php"><font size="+1" color="#000000"><b> Menu </b></font></a></td>
+        <td width="20%"><a class="button-2" href="#"><font size="+1" color="#000000"><b> Promotion And Discount </b></font></a></td>
+        <td width="20%"><a class="button-2" href="billing.php"><font size="+1" color="#000000"><b> Billing </b></font></a></td>
+        <td width="20%"><a class="button-2" href="forum.php"><font size="+1" color="#000000"><b> Forum </b></font></a></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+	</td>
+  </tr>
+  <tr>
+    <td width="20%" valign="top">
+		<table width="100%" border="0">
+  <tr>
+    <td bgcolor="#33CCFF"><div align="center"><strong><font size="+2">Special Offer !!!</font></strong></div></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><div align="center"><font size="+1">We will give discount until 40% off until the end of this year</font></div></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><img src="images/banner-2.jpg" width="170" height="90" /></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#33CCFF"><div align="center"><strong><font size="+2">New Menus</font></strong></div></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><img src="images/page2-img10.jpg" width="170" height="90" /></td>
+  </tr>
+  <tr>
+    <td><img src="images/page3-img3.jpg" width="170" height="90" /></td>
+  </tr>
+</table>
+	</td>
+	
+    <td width="60%" valign="top">
+		<table width="100%" border="0">
+  <tr>
+    <td>
+		<header>
+			<div class="row-bot">
+				<div class="row-bot-bg">
+					<div class="main">
+						<div class="slider-wrapper">
+							<div class="slider">
+								<ul class="items">
+									<li>
+										<img src="images/slider-img1.jpg" alt="" />
+									</li>
+									<li>
+										<img src="images/slider-img2.jpg" alt="" />	
+									</li>
+									<li>
+										<img src="images/slider-img3.jpg" alt="" />									
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	</td>
+  </tr>
+</table>
+	</td>
+	
+    <td width="20%" valign="top">
+		<table width="100%" border="0">
+  <tr>
+    <td colspan="2" bgcolor="#00CCFF"><div align="center"><strong><font size="+2">Our Service</font></strong></div></td>
+  </tr>
+  <tr>
+    <td colspan="2">
+		<ul>
+			<li><font size="+1">bla bla bla</font></li>
+			<li><font size="+1">bla bla bla</font></li>
+			<li><font size="+1">bla bla bla</font></li>
+			<li><font size="+1">bla bla bla</font></li>
+			<li><font size="+1">bla bla bla</font></li>
+			<li><font size="+1">bla bla bla</font></li>
+		</ul>
+	</td>
+  </tr>
+  <tr>
+  	<td> <img src="images/index.jpeg" width="80" height="50"> </td>
+	<td> <img src="images/bii.jpeg" width="80" height="50"/> </td>
+  </tr>
+  <tr>
+  	<td colspan="2">&nbsp;  </td>
+  </tr>
+  <tr>
+  	<td> <img src="images/danamon.jpeg" width="80" height="50"> </td>
+	<td> <img src="images/niaga.jpeg" width="80" height="50"/> </td>
+  </tr>
+</table>
+	</td>
+  </tr>
+</table>
+
+	<script type="text/javascript"> Cufon.now(); </script>
+    <script type="text/javascript">
+		$(window).load(function() {
+			$('.slider')._TMS({
+				duration:1000,
+				easing:'easeOutQuint',
+				preset:'slideDown',
+				slideshow:7000,
+				banners:false,
+				pauseOnHover:true,
+				pagination:true,
+				pagNums:false
+			});
+		});
+    </script>
+</body>
+</html>
